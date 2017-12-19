687b6041f8561590e2ecd72a52280fc924fe5201
Jay G <playerjayho@gmail.com>
2017-12-19 19:34:23 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -4,16 +4,16 @@ $(document).ready(function(){
 
 // call functions here
   function getIt(){
-    
+
   }
   function frameIt(){
-    
+
   }
   function pressIt(){
-    
+
   }
   function submitIt(){
-    
+
   }
-  
+
 });
