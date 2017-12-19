5799079b5e45237f440d47adeaab017418a7aa1a
Jay G <playerjayho@gmail.com>
2017-12-19 19:48:41 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -18,7 +18,7 @@ function pressIt(){
 
 function submitIt(){
   $("form").on("submit", function(){
-      alert("Your form is going to be submitted now");
+      alert("Your form is going to be submitted now.");
   });
 }
 
