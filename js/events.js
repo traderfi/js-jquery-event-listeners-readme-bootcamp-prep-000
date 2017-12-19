7135a3dc9540520a05224657b83f241b26548986
Jay G <playerjayho@gmail.com>
2017-12-19 19:56:13 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,39 @@
 //define functions here
+function submitIt(){
+  $("form").on("submit", function(){
+      alert("Your form is going to be submitted now.");
+  });
+}
+
+function getIt(){
+  $('p').on('click', function(){
+    alert("Hey!");
+  });
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+      $('img').addClass('tasty');
+  });
+}
+
+function pressIt(){
+  $('form #typing').on('keydown', function(e){
+      if(e.which == 71){
+          alert('asds');
+      }
+  });
+}
+
+
 
 $(document).ready(function(){
 
 // call functions here
+
+
+
+
+
 
 });
